647931a9b1afd134eb4f37c7de05f6107aa2b987
Todd Rimes <todd@rimesmedia.com>
2021-05-02 11:54:44 -0700

    safety, before rewriting htmlIterator

--- index.php
+++ index.php
@@ -54,7 +54,7 @@ function transform($xml,$xsl,$timeString) {
  * @param SimpleXMLElement $xml
  * @return array
  */
-function xmlToArray(SimpleXMLElement $xml)
+/*function xmlToArray(SimpleXMLElement $xml)
 {
     $parser = function (SimpleXMLElement $xml, array $collection = []) use (&$parser) {
         $nodes = $xml->children();
@@ -86,7 +86,7 @@ function xmlToArray(SimpleXMLElement $xml)
     return [
         $xml->getName() => $parser($xml)
     ];
-}
+}*/
 
 $inputJSON = file_get_contents('php://input');
 $input = json_decode($inputJSON, TRUE); //convert JSON into array
@@ -96,45 +96,110 @@ $xslFile = "";
 
 $milliseconds = getTimeString();
 
-if (filter_var($input["xml"], FILTER_VALIDATE_URL)) {
-    // Use basename() function to return the base name of file
-    $file_name = basename($input["xml"]);
-    $xmlFile = $milliseconds. ".xml";
-    // Use file_get_contents() function to get the file
-    // from url and use file_put_contents() function to
-    // save the file by using base name
-    if(file_put_contents( $xmlFile,file_get_contents($input["xml"]))) {
-        // echo "XML downloaded successfully";
+function setXmlFile() {
+    global $milliseconds, $input;
+    $file_name = $milliseconds. ".xml";
+    if (filter_var($input["xml"], FILTER_VALIDATE_URL)) {
+        if(!file_put_contents( $file_name,file_get_contents($input["xml"]))) {
+            echo "XML downloading failed.";
+        }
+    } else {
+        if (!copy($input["xml"], $file_name)) {
+            echo "failed to copy " . $input['xml'] . "...\n";
+            return false;
+        }
     }
-    else {
-        echo "XML downloading failed.";
-    }
-} else {
-    if (!copy($input["xml"], $milliseconds. ".xml")) {
-        echo "failed to copy " . $input['xml'] . "...\n";
-    }
-    $xmlFile = $milliseconds. ".xml";
+    return $file_name;
 }
 
-if (filter_var($input["xsl"], FILTER_VALIDATE_URL)) {
-    // Use basename() function to return the base name of file
-    $file_name = basename($input["xsl"]);
-    $xslFile = $milliseconds. ".xsl";
-
-    // Use file_get_contents() function to get the file
-    // from url and use file_put_contents() function to
-    // save the file by using base name
-    if(file_put_contents( $xslFile,file_get_contents($input["xsl"]))) {
-        // echo "XSL downloaded successfully";
+function setXslFile() {
+    global $milliseconds, $input;
+    $file_name = $milliseconds. ".xsl";
+    if (filter_var($input["xsl"], FILTER_VALIDATE_URL)) {
+        if(!file_put_contents( $file_name,file_get_contents($input["xsl"]))) {
+            echo "XSL downloading failed.";
+        }
+    } else {
+        if (!copy($input["xsl"], $file_name)) {
+            echo "failed to copy " . $input['xsl'] . "...\n";
+            return false;
+        }
     }
-    else {
-        echo "XSL downloading failed.";
-    }
-} else {
-    if (!copy($input["xsl"], $milliseconds. ".xsl")) {
-        echo "failed to copy " . $input['xsl'] . "...\n";
-    }
-    $xslFile = $milliseconds. ".xsl";
+    return $file_name;
 }
 
+function htmlIterator($str)
+{
+    $offset = 0;
+    $i = 0;
+    while ( preg_match_all('/<(p|h1|h2|h3|figure).?>(.+?)<\/(p|h1|h2|h3|figure)>/', $str, $m, PREG_OFFSET_CAPTURE, $offset)) {
+        $result = new stdClass();
+        $offset = $m[0][1] + strlen($m[0][0]);
+        $result->tag = $m[1][$i][0];
+        $result->content = $m[2][$i][0];
+        $i++;
+        yield $result;
+    }
+}
+
+function splitStories($timeString) {
+    $json = file_get_contents($timeString. "-OUT.json", FILE_USE_INCLUDE_PATH);
+    $objectFeed = json_decode($json);
+    foreach ($objectFeed->channel->item as &$story) {
+        $cleanStoryId = stripslashes($story->identifier);
+        preg_match("/\/(.+)$/",$cleanStoryId,$matches);
+        $guid=$matches[1];
+        $guid = ltrim($guid, '/');
+        $guid = str_replace('/','_',$guid);
+        $components = array();
+        foreach ($story->components->element as $component) {
+            $name = "@attributes";
+            if (!$component->$name) {
+                array_push($components, $component);
+            } else {
+                $html = $component->html;
+                foreach( htmlIterator($html) as $element) {
+                    $newComponent = stdClass();
+                    switch($element->tag) {
+                        case 'h1':
+                            $newComponent->role="heading1";
+                            $newComponent->text=$element->content;
+                            break;
+                        case 'h2':
+                            $newComponent->role="heading2";
+                            $newComponent->text=$element->content;
+                            break;
+                        case 'h3':
+                            $newComponent->role="heading3";
+                            $newComponent->text=$element->content;
+                            break;
+                        case 'figure':
+                            $newComponent->role="figure";
+                            preg_match_all('/\b(?:(?:https?|ftp|file):\/\/|www\.|ftp\.)[-A-Z0-9+&@#\/%=~_|$?!:,.]*[A-Z0-9+&@#\/%=~_|$]/i', $element[1], $result, PREG_PATTERN_ORDER);
+                            $newComponent->URL=$result->content;
+                            break;
+                        default:
+                            $newComponent->role="body";
+                            $newComponent->text=$element->content;
+                            break;
+                    }
+                    array_push($components, $newComponent);
+                }
+            }
+        }
+        $story->components = $components;
+        file_put_contents($guid,json_encode($story));
+    }
+}
+
+function cleanup($timeString) {
+    unlink($timeString . ".xml");
+    unlink($timeString . ".xsl");
+    unlink($timeString . "-OUT.xml");
+}
+
+$xmlFile = setXmlFile();
+$xslFile = setXslFile();
 transformLocal($xmlFile,$xslFile,$milliseconds);
+cleanup($milliseconds);
+splitStories($milliseconds);
